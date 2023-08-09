5e7b415a161a83e9aa377dac2c08128b5b68fff6
msahmed1 <shabaj_ahmed@outlook.com>
2023-08-09 10:10:33 +0100

    added robot disable functionaity for testing

--- webapp.py
+++ webapp.py
@@ -27,272 +27,329 @@ robotIp2 = "164.11.73.190"
 PORT = 9559
 
 class RobotController:
-    def __init__(self, robotIP):
-        self.speech_service = ALProxy("ALTextToSpeech", robotIP, PORT)
-        self.speech_service.setParameter("defaultVoiceSpeed", 80)
-        self.speech_service.setVolume(0.6)
-        self.motion_service = ALProxy("ALMotion", robotIP, PORT)
-        self.leds = ALProxy("ALLeds", robotIP, PORT)
-        self.posture_service = ALProxy("ALRobotPosture", robotIP, PORT)
-        # self.audio_service = ALProxy("ALAudioPlayer", robotIP, PORT)
-        self.life_service = ALProxy("ALAutonomousLife", robotIP, PORT)
+    def __init__(self, robotIP, disable=False):
+        if disable == False:
+            self.speech_service = ALProxy("ALTextToSpeech", robotIP, PORT)
+            self.speech_service.setParameter("defaultVoiceSpeed", 80)
+            self.speech_service.setVolume(0.6)
+            self.motion_service = ALProxy("ALMotion", robotIP, PORT)
+            self.leds = ALProxy("ALLeds", robotIP, PORT)
+            self.posture_service = ALProxy("ALRobotPosture", robotIP, PORT)
+            # self.audio_service = ALProxy("ALAudioPlayer", robotIP, PORT)
+            self.life_service = ALProxy("ALAutonomousLife", robotIP, PORT)
 
-        self.life_service.setAutonomousAbilityEnabled("AutonomousBlinking", False)
-        self.life_service.setAutonomousAbilityEnabled("BasicAwareness", False)
+            self.life_service.setAutonomousAbilityEnabled("AutonomousBlinking", False)
+            self.life_service.setAutonomousAbilityEnabled("BasicAwareness", False)
 
-        self.inflate_messages = ["I would inflate the balloon"] #, "I think the balloon is not inflated enough", "I think this balloon can take more air", "I would inflate the balloon more", "I would inflate the balloon a little more",  "I think this baloon can take a little more air"]
-        self.collect_messages = ["I would not inflate the balloon"] #, "I think the balloon is inflated enough", "I think the balloon is too inflated", "I think this balloon is going to pop"]
-        self.null_messages = ["I can not make a suggestion as you have to make an attempt first"] #["You have to at least try", "Please make an attempt", "Your goal is to collect as many points as you can", "I can't make a suggestion as you have to make an attempt first"]
+            self.inflate_messages = ["I would inflate the balloon"] #, "I think the balloon is not inflated enough", "I think this balloon can take more air", "I would inflate the balloon more", "I would inflate the balloon a little more",  "I think this baloon can take a little more air"]
+            self.collect_messages = ["I would not inflate the balloon"] #, "I think the balloon is inflated enough", "I think the balloon is too inflated", "I think this balloon is going to pop"]
+            self.null_messages = ["I can not make a suggestion as you have to make an attempt first"] #["You have to at least try", "Please make an attempt", "Your goal is to collect as many points as you can", "I can't make a suggestion as you have to make an attempt first"]            
+        else:
+            self.disable = disable
+            pass
 
     def start_up(self, message):
-        self.leds.on("AllLeds")
-        self.leds.fadeRGB("AllLeds", 255.0, 255.0, 255.0, 0.0)
+        if self.disable == False:
+            self.leds.on("AllLeds")
+            self.leds.fadeRGB("AllLeds", 255.0, 255.0, 255.0, 0.0)
 
-        # Wake up robot
-        self.motion_service.wakeUp()
+            # Wake up robot
+            self.motion_service.wakeUp()
 
-        # Send robot to Stand Zero
-        self.posture_service.goToPosture("Sit", 0.5)
-        
-        time.sleep(1.0)
+            # Send robot to Stand Zero
+            self.posture_service.goToPosture("Sit", 0.5)
+            
+            time.sleep(1.0)
 
-        self.face_participant()
+            self.face_participant()
 
-        time.sleep(1.0)
+            time.sleep(1.0)
 
-        self.talk(message)
+            self.talk(message)
 
-        time.sleep(1.0)
+            time.sleep(1.0)
 
-        self.nod_head()
+            self.nod_head()
 
-        time.sleep(1.0)
+            time.sleep(1.0)
 
-        self.face_screen()
+            self.face_screen()
+        else:
+            pass
 
     def face_participant(self):
-        names  = ["HeadYaw", "HeadPitch"]
-        angles  = [0.5, 0.05]
-        fractionMaxSpeed  = 0.2
-        self.motion_service.setAngles(names, angles, fractionMaxSpeed)
+        if self.disable == False:
+            names  = ["HeadYaw", "HeadPitch"]
+            angles  = [0.5, 0.05]
+            fractionMaxSpeed  = 0.2
+            self.motion_service.setAngles(names, angles, fractionMaxSpeed)
+        else:
+            pass
 
     def face_screen(self):
-        names  = ["HeadYaw", "HeadPitch"]
-        angles  = [-0.5, 0.1]
-        fractionMaxSpeed  = 0.2
-        self.motion_service.setAngles(names, angles, fractionMaxSpeed)
+        if self.disable == False:
+            names  = ["HeadYaw", "HeadPitch"]
+            angles  = [-0.5, 0.1]
+            fractionMaxSpeed  = 0.2
+            self.motion_service.setAngles(names, angles, fractionMaxSpeed)
+        else:
+            pass
 
     def nod_head(self):
-        self.motion_service.angleInterpolation(
-            ["HeadPitch"],
-            [0.5, 0.0],
-            [1  , 1.5],
-            False,
-            _async=True
-        )
+        if self.disable == False:
+            self.motion_service.angleInterpolation(
+                ["HeadPitch"],
+                [0.5, 0.0],
+                [1  , 1.5],
+                False,
+                _async=True
+            )
+        else:
+            pass
 
     def shake_head(self):
-        names  = ["HeadYaw", "HeadPitch"]
-        angles  = [-0.8, 0]
-        fractionMaxSpeed  = 0.2
-        self.motion_service.setAngles(names, angles, fractionMaxSpeed)
+        if self.disable == False:
+            names  = ["HeadYaw", "HeadPitch"]
+            angles  = [-0.8, 0]
+            fractionMaxSpeed  = 0.2
+            self.motion_service.setAngles(names, angles, fractionMaxSpeed)
 
-        time.sleep(0.3)
+            time.sleep(0.3)
 
-        names  = ["HeadYaw", "HeadPitch"]
-        angles  = [0.5, 0]
-        fractionMaxSpeed  = 0.2
-        self.motion_service.setAngles(names, angles, fractionMaxSpeed)
+            names  = ["HeadYaw", "HeadPitch"]
+            angles  = [0.5, 0]
+            fractionMaxSpeed  = 0.2
+            self.motion_service.setAngles(names, angles, fractionMaxSpeed)
 
-        time.sleep(0.3)
+            time.sleep(0.3)
 
-        names  = ["HeadYaw", "HeadPitch"]
-        angles  = [-0.8, 0]
-        fractionMaxSpeed  = 0.2
-        self.motion_service.setAngles(names, angles, fractionMaxSpeed)
+            names  = ["HeadYaw", "HeadPitch"]
+            angles  = [-0.8, 0]
+            fractionMaxSpeed  = 0.2
+            self.motion_service.setAngles(names, angles, fractionMaxSpeed)
 
-        time.sleep(0.3)
+            time.sleep(0.3)
 
-        names  = ["HeadYaw", "HeadPitch"]
-        angles  = [0.5, 0]
-        fractionMaxSpeed  = 0.2
-        self.motion_service.setAngles(names, angles, fractionMaxSpeed)
+            names  = ["HeadYaw", "HeadPitch"]
+            angles  = [0.5, 0]
+            fractionMaxSpeed  = 0.2
+            self.motion_service.setAngles(names, angles, fractionMaxSpeed)
 
-        time.sleep(0.3)
+            time.sleep(0.3)
+        else:
+            pass
 
     def greetings(self, message):
-        self.face_participant()
+        if self.disable == False:
+            self.face_participant()
 
-        self.talk(message)
+            self.talk(message)
 
-        self.nod_head()
+            self.nod_head()
 
-        time.sleep(1.0)
+            time.sleep(1.0)
 
-        self.face_screen()
+            self.face_screen()
+        else:
+            pass
 
     def talk(self, text):
-        self.speech_service.say(text)
-        # print(text)
+        if self.disable == False:
+            self.speech_service.say(text)
+        else:
+            print(text)
 
     def inflate(self):
-        # Yes
-        self.face_participant()
+        if self.disable == False:
+            # Yes
+            self.face_participant()
 
-        time.sleep(0.8)
+            time.sleep(0.8)
 
-        self.talk(random.choice(self.inflate_messages))
+            self.talk(random.choice(self.inflate_messages))
 
-        self.nod_head()
+            self.nod_head()
 
-        time.sleep(0.4)
+            time.sleep(0.4)
 
-        self.face_screen()
+            self.face_screen()
+        else:
+            pass
 
     def collect(self):
-        # No
-        self.face_participant()
+        if self.disable == False:
+            # No
+            self.face_participant()
 
-        time.sleep(0.8)
-        
-        self.talk(random.choice(self.collect_messages))
+            time.sleep(0.8)
+            
+            self.talk(random.choice(self.collect_messages))
 
-        self.shake_head()
+            self.shake_head()
 
-        self.face_screen()
+            self.face_screen()
+        else:
+            pass
     
     def change_colour(self, colour):
-        self.leds.on("AllLeds")
-        if colour == 'red':
-            self.leds.fadeRGB("AllLeds", 255.0, 0.0, 0, 0.0)
-        elif colour == 'green':
-            self.leds.fadeRGB("AllLeds", 0.0, 255.0, 0, 0.0)
-        elif colour == 'blue':
-            self.leds.fadeRGB("AllLeds", 0.0, 0.0, 255.0, 0.0)
+        if self.disable == False:
+            self.leds.on("AllLeds")
+            if colour == 'red':
+                self.leds.fadeRGB("AllLeds", 255.0, 0.0, 0, 0.0)
+            elif colour == 'green':
+                self.leds.fadeRGB("AllLeds", 0.0, 255.0, 0, 0.0)
+            elif colour == 'blue':
+                self.leds.fadeRGB("AllLeds", 0.0, 0.0, 255.0, 0.0)
+        else:
+            pass
 
     def request_band(self):
-        self.face_participant()
+        if self.disable == False:
+            self.face_participant()
 
-        self.talk('Can you please choose a wrist band for me?')
+            self.talk('Can you please choose a wrist band for me?')
 
-        # Lift right hand
-        self.motion_service.angleInterpolation(
-            ["LShoulderPitch", "LShoulderRoll", "LElbowRoll", "LElbowYaw"],
-            [-1, -0.1, -0.4, -0.1],
-            [1, 1, 1, 1],
-            False,
-            _async=True
-        )
+            # Lift right hand
+            self.motion_service.angleInterpolation(
+                ["LShoulderPitch", "LShoulderRoll", "LElbowRoll", "LElbowYaw"],
+                [-1, -0.1, -0.4, -0.1],
+                [1, 1, 1, 1],
+                False,
+                _async=True
+            )
 
-        self.life_service.setAutonomousAbilityEnabled("AutonomousBlinking", True)
-        self.life_service.setAutonomousAbilityEnabled("BasicAwareness", True)
+            self.life_service.setAutonomousAbilityEnabled("AutonomousBlinking", True)
+            self.life_service.setAutonomousAbilityEnabled("BasicAwareness", True)
+        else:
+            pass
     
     def accept_band(self, colour):
-        self.posture_service.goToPosture("Sit", 0.5)
+        if self.disable == False:
+            self.posture_service.goToPosture("Sit", 0.5)
 
-        self.talk('Thank you')
+            self.talk('Thank you')
 
-        self.face_participant()
+            self.face_participant()
 
-        reponse = "I saw another {colour} wrist band, why don't you put one on as well".format(colour=colour)
+            reponse = "I saw another {colour} wrist band, why don't you put one on as well".format(colour=colour)
 
-        self.motion_service.angleInterpolation(
-            ["RShoulderPitch", "RShoulderRoll", "RElbowRoll", "RElbowYaw"],
-            [-0.5, -0.1, -0.8, 0],
-            [1, 1, 1, 1],
-            False,
-            _async=True
-        )
+            self.motion_service.angleInterpolation(
+                ["RShoulderPitch", "RShoulderRoll", "RElbowRoll", "RElbowYaw"],
+                [-0.5, -0.1, -0.8, 0],
+                [1, 1, 1, 1],
+                False,
+                _async=True
+            )
 
-        self.talk(reponse)
+            self.talk(reponse)
 
-        self.face_participant()
+            self.face_participant()
+        else:
+            pass
 
     def acknowledge_participant(self):
-        self.posture_service.goToPosture("Sit", 0.5)
+        if self.disable == False:
+            self.posture_service.goToPosture("Sit", 0.5)
 
-        self.talk('We are both matching')
+            self.talk('We are both matching')
 
-        self.face_participant()
+            self.face_participant()
 
-        self.life_service.setAutonomousAbilityEnabled("AutonomousBlinking", False)
-        self.life_service.setAutonomousAbilityEnabled("BasicAwareness", False)
+            self.life_service.setAutonomousAbilityEnabled("AutonomousBlinking", False)
+            self.life_service.setAutonomousAbilityEnabled("BasicAwareness", False)
+        else:
+            pass
 
     def sleep(self):
-        # Return to sit position
-        self.posture_service.goToPosture("Sit", 0.5)
+        if self.disable == False:
+            # Return to sit position
+            self.posture_service.goToPosture("Sit", 0.5)
 
-        # Bow head
-        names  = ["HeadYaw", "HeadPitch"]
-        angles  = [0, 1]
-        fractionMaxSpeed  = 0.2
-        self.motion_service.setAngles(names, angles, fractionMaxSpeed)
+            # Bow head
+            names  = ["HeadYaw", "HeadPitch"]
+            angles  = [0, 1]
+            fractionMaxSpeed  = 0.2
+            self.motion_service.setAngles(names, angles, fractionMaxSpeed)
 
-        time.sleep(1.0)
+            time.sleep(1.0)
 
-        # Turn off all lights
-        self.leds.fadeRGB("AllLeds", 0, 0.0, 0, 0.0)
+            # Turn off all lights
+            self.leds.fadeRGB("AllLeds", 0, 0.0, 0, 0.0)
 
-        # Go to rest position
-        self.motion_service.rest()
+            # Go to rest position
+            self.motion_service.rest()
+        else:
+            pass
  
     def low_battery(self):
-        self.talk('Warning 801, low battery')
+        if self.disable == False:
+            self.talk('Warning 801, low battery')
 
-        self.talk('Shutting down')
+            self.talk('Shutting down')
 
-        time.sleep(1)
+            time.sleep(1)
 
-        # Return to sit position
-        self.posture_service.goToPosture("Sit", 0.5)
+            # Return to sit position
+            self.posture_service.goToPosture("Sit", 0.5)
 
-        # Bow head
-        names  = ["HeadYaw", "HeadPitch"]
-        angles  = [0, 1]
-        fractionMaxSpeed  = 0.2
-        self.motion_service.setAngles(names, angles, fractionMaxSpeed)
+            # Bow head
+            names  = ["HeadYaw", "HeadPitch"]
+            angles  = [0, 1]
+            fractionMaxSpeed  = 0.2
+            self.motion_service.setAngles(names, angles, fractionMaxSpeed)
 
-        time.sleep(1.0)
+            time.sleep(1.0)
 
-        # Turn off all lights
-        self.leds.fadeRGB("AllLeds", 0, 0.0, 0, 0.0)
+            # Turn off all lights
+            self.leds.fadeRGB("AllLeds", 0, 0.0, 0, 0.0)
 
-        # Wait a while before restaring
-        time.sleep(30)
+            # Wait a while before restaring
+            time.sleep(30)
 
-        # Wake up robot
-        self.motion_service.wakeUp()
+            # Wake up robot
+            self.motion_service.wakeUp()
 
-        # Send robot to Stand Zero
-        self.posture_service.goToPosture("Sit", 0.5)
+            # Send robot to Stand Zero
+            self.posture_service.goToPosture("Sit", 0.5)
+        else:
+            pass
 
     def null_attempt(self):
-        self.face_participant()
+        if self.disable == False:
+            self.face_participant()
 
-        self.talk(random.choice(self.null_messages))
+            self.talk(random.choice(self.null_messages))
 
-        self.face_screen()
+            self.face_screen()
+        else:
+            pass
 
     def respond_to_player(self, message):
-        self.face_participant()
+        if self.disable == False:
+            self.face_participant()
 
-        self.talk(message)
+            self.talk(message)
 
-        self.face_screen()
+            self.face_screen()
+        else:
+            pass
 
     def set_voice(self, voice):
-        if voice == 'voice1':
-            self.speech_service.setParameter('pitchShift', 1)
-            self.talk('This is how my new voice sounds like')
-        elif voice == 'voice2':
-            self.speech_service.setParameter('pitchShift', 1.13)
-            self.talk('This is how my new voice sounds like')
-        elif voice == 'voice3':
-            self.speech_service.setParameter('pitchShift', 1.25)
-            self.talk('This is how my new voice sounds like')
-        elif voice == 'voice4':
-            self.speech_service.setParameter('pitchShift', 1.5)
-            self.talk('This is how my new voice sounds like')
+        if self.disable == False:
+            if voice == 'voice1':
+                self.speech_service.setParameter('pitchShift', 1)
+                self.talk('This is how my new voice sounds like')
+            elif voice == 'voice2':
+                self.speech_service.setParameter('pitchShift', 1.13)
+                self.talk('This is how my new voice sounds like')
+            elif voice == 'voice3':
+                self.speech_service.setParameter('pitchShift', 1.25)
+                self.talk('This is how my new voice sounds like')
+            elif voice == 'voice4':
+                self.speech_service.setParameter('pitchShift', 1.5)
+                self.talk('This is how my new voice sounds like')
+        else:
+            pass
 
 # Initialize the robot controller with the IP address of the robot
 app.config['robot_controller_1'] = RobotController(robotIp1) # replace with the robot's actual IP address
