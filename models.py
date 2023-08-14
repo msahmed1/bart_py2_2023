f6e376e7094fbffc3bc4db073a11cad9bcf86a42
msahmed1 <shabaj_ahmed@outlook.com>
2023-08-14 15:44:48 +0100

    Updated SQL database schema

--- models.py
+++ models.py
@@ -81,8 +81,14 @@ class GameRoundSurvey(db.Model):
         db.Integer)
     my_robot_provides_very_sensible_and_effective_advice_if_needed = db.Column(
         db.Integer)
+    
+    i_can_decide_more_quickly_and_easily_which_action_to_take_than_without_using_this_robotic_advisor = db.Column(db.Integer)
+    i_can_make_better_decisions_than_without_using_this_robotic_advisor = db.Column(db.Integer)
+    i_am_better_informed_about_what_action_to_take_than_without_using_this_robotic_advisor = db.Column(db.Integer)
+    i_can_make_more_accurate_decisions_than_without_using_this_robotic_advisor = db.Column(db.Integer)
+    i_can_better_decide_whether_i_want_to_inflate_the_balloon_or_not = db.Column(db.Integer)
 
-    def __init__(self, player_id, game_round=0, total_score=0, dislike_like=None, unfriendly_friendly=None, unkind_kind=None, unpleasant_pleasant=None, awful_nice=None, incompetent_competent=None, ignorant_knowledgeable=None, iriresponsible_responsible=None, unitelligent_intelligent=None, foolish_sensible=None, this_is_my_robot=None, i_feel_high_degree_of_personal_ownership_for_this_robot=None, i_sense_that_i_own_this_robot=None, robot_incorporates_a_part_of_myself=None, my_robot_supplies_my_need_for_help_through_a_help_function=None, my_robot_provides_competent_guidanceas_through_a_help_function=None, my_robot_provides_very_sensible_and_effective_advice_if_needed=None):
+    def __init__(self, player_id, game_round=0, total_score=0, dislike_like=None, unfriendly_friendly=None, unkind_kind=None, unpleasant_pleasant=None, awful_nice=None, incompetent_competent=None, ignorant_knowledgeable=None, iriresponsible_responsible=None, unitelligent_intelligent=None, foolish_sensible=None, this_is_my_robot=None, i_feel_high_degree_of_personal_ownership_for_this_robot=None, i_sense_that_i_own_this_robot=None, robot_incorporates_a_part_of_myself=None, my_robot_supplies_my_need_for_help_through_a_help_function=None, my_robot_provides_competent_guidanceas_through_a_help_function=None, my_robot_provides_very_sensible_and_effective_advice_if_needed=None, i_can_decide_more_quickly_and_easily_which_action_to_take_than_without_using_this_robotic_advisor=None, i_can_make_better_decisions_than_without_using_this_robotic_advisor=None, i_am_better_informed_about_what_action_to_take_than_without_using_this_robotic_advisor=None, i_can_make_more_accurate_decisions_than_without_using_this_robotic_advisor=None, i_can_better_decide_whether_i_want_to_inflate_the_balloon_or_not=None):
         super(GameRoundSurvey, self).__init__()
         self.player_id = player_id
         self.game_round = game_round
@@ -108,3 +114,10 @@ class GameRoundSurvey(db.Model):
         self.my_robot_supplies_my_need_for_help_through_a_help_function = my_robot_supplies_my_need_for_help_through_a_help_function
         self.my_robot_provides_competent_guidanceas_through_a_help_function = my_robot_provides_competent_guidanceas_through_a_help_function
         self.my_robot_provides_very_sensible_and_effective_advice_if_needed = my_robot_provides_very_sensible_and_effective_advice_if_needed
+
+        self.i_can_decide_more_quickly_and_easily_which_action_to_take_than_without_using_this_robotic_advisor = i_can_decide_more_quickly_and_easily_which_action_to_take_than_without_using_this_robotic_advisor
+        self.i_can_make_better_decisions_than_without_using_this_robotic_advisor = i_can_make_better_decisions_than_without_using_this_robotic_advisor
+        self.i_am_better_informed_about_what_action_to_take_than_without_using_this_robotic_advisor = i_am_better_informed_about_what_action_to_take_than_without_using_this_robotic_advisor
+        self.i_can_make_more_accurate_decisions_than_without_using_this_robotic_advisor = i_can_make_more_accurate_decisions_than_without_using_this_robotic_advisor
+        self.i_can_better_decide_whether_i_want_to_inflate_the_balloon_or_not = i_can_better_decide_whether_i_want_to_inflate_the_balloon_or_not
+        
