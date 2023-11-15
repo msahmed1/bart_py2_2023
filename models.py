e527b497edc8a53804141e273ff559eae7f133da
msahmed1 <shabaj_ahmed@outlook.com>
2023-11-15 14:23:22 +0000

    Updated database schema

--- models.py
+++ models.py
@@ -13,17 +13,15 @@ class Players(db.Model):
     consent = db.Column(db.Boolean)
     game_completed = db.Column(db.Boolean, default=False)
 
-    freetext = db.Column(db.Text, nullable=True)
-
     # Demographics
     age = db.Column(db.Integer)
     gender = db.Column(db.String(20))
-    
+
     # Relationships
     balloon_total_inflates = db.relationship(
         'BalloonInflate', backref='player', lazy=True)
     game_round_surveys = db.relationship(
-        'GameRoundSurvey', backref='player', lazy=True)
+        'FinalSurvey', backref='player', lazy=True)
 
     def __init__(self, player_id, customise_first, testing, consent=False):
         self.player_id = player_id
@@ -48,7 +46,8 @@ class BalloonInflate(db.Model):
     total_inflates_before_help_request = db.Column(db.Integer, default=0)
     # Calculate inflates_after_help_request by subtracting inflates_before_help_request from total_inflates
 
-    time_to_decide = db.Column(db.Float)
+    time_to_decide = db.Column(db.Float, default=0)
+
     def __init__(self, player_id, balloon_id, balloon_limit, total_inflates=0, game_round=0, help_requested=False, robot_response=False, inflated_after_help_request=False):
         super(BalloonInflate, self).__init__()
         self.player_id = player_id
@@ -61,38 +60,39 @@ class BalloonInflate(db.Model):
         self.robot_response = robot_response
         self.inflated_after_help_request = inflated_after_help_request
 
-class GameRoundSurvey(db.Model):
+
+class FinalSurvey(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    player_id = db.Column(db.Integer, db.ForeignKey('players.player_id'), nullable=False)
-    game_round = db.Column(db.Integer, default=0)
-    
-    total_score = db.Column(db.Integer)
+    player_id = db.Column(db.Integer, db.ForeignKey(
+        'players.player_id'), nullable=False)
 
-    dislike_like = db.Column(db.Integer)
-    unfriendly_friendly = db.Column(db.Integer)
-    unkind_kind = db.Column(db.Integer)
-    unpleasant_pleasant = db.Column(db.Integer)
-    awful_nice = db.Column(db.Integer)
+    dislike_like = db.Column(db.Integer, default=0)
+    unfriendly_friendly = db.Column(db.Integer, default=0)
+    unkind_kind = db.Column(db.Integer, default=0)
+    unpleasant_pleasant = db.Column(db.Integer, default=0)
+    awful_nice = db.Column(db.Integer, default=0)
 
-    incompetent_competent = db.Column(db.Integer)
-    ignorant_knowledgeable = db.Column(db.Integer)
-    iriresponsible_responsible = db.Column(db.Integer)
-    unitelligent_intelligent = db.Column(db.Integer)
-    foolish_sensible = db.Column(db.Integer)
+    incompetent_competent = db.Column(db.Integer, default=0)
+    ignorant_knowledgeable = db.Column(db.Integer, default=0)
+    iriresponsible_responsible = db.Column(db.Integer, default=0)
+    unitelligent_intelligent = db.Column(db.Integer, default=0)
+    foolish_sensible = db.Column(db.Integer, default=0)
 
-    this_is_my_robot = db.Column(db.Integer)
-    i_sense_that_i_own_this_robot = db.Column(db.Integer)
-    this_robot_incorporates_a_part_of_myself = db.Column(db.Integer)
+    this_is_my_robot = db.Column(db.Integer, default=0)
+    i_sense_that_i_own_this_robot = db.Column(db.Integer, default=0)
+    this_robot_incorporates_a_part_of_myself = db.Column(db.Integer, default=0)
 
-    i_am_confident_in_the_robot = db.Column(db.Integer)
-    the_robot_has_integrity = db.Column(db.Integer)
-    the_robot_gave_good_advice = db.Column(db.Integer)
-    the_robot_is_reliable = db.Column(db.Integer)
-    i_can_trust_the_robot = db.Column(db.Integer)
-    i_am_familiar_with_the_robot = db.Column(db.Integer)
+    i_am_confident_in_the_robot = db.Column(db.Integer, default=0)
+    the_robot_has_integrity = db.Column(db.Integer, default=0)
+    the_robot_gave_good_advice = db.Column(db.Integer, default=0)
+    the_robot_is_reliable = db.Column(db.Integer, default=0)
+    i_can_trust_the_robot = db.Column(db.Integer, default=0)
+    i_am_familiar_with_the_robot = db.Column(db.Integer, default=0)
+
+    freetext = db.Column(db.Text, nullable=True)
 
     def __init__(self, player_id, game_round=0, total_score=0, dislike_like=None, unfriendly_friendly=None, unkind_kind=None, unpleasant_pleasant=None, awful_nice=None, incompetent_competent=None, ignorant_knowledgeable=None, iriresponsible_responsible=None, unitelligent_intelligent=None, foolish_sensible=None, this_is_my_robot=None, i_sense_that_i_own_this_robot=None, this_robot_incorporates_a_part_of_myself=None, i_am_confident_in_the_robot=None, the_robot_has_integrity=None, the_robot_gave_good_advice=None, the_robot_is_reliable=None, i_can_trust_the_robot=None, i_am_familiar_with_the_robot=None):
-        super(GameRoundSurvey, self).__init__()
+        super(FinalSurvey, self).__init__()
         self.player_id = player_id
         self.game_round = game_round
         self.total_score = total_score
